519c052f7fad55e2e8b9423654c65518cfdd0b90
Crowdbotics <team@crowdbotics.com>
2020-08-19 22:57:24 +0000

    remove package blueprint-react-native-settings-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -12,7 +12,6 @@ export const installed_blueprints = [
 { name: 'UserProfile88516', human_name: 'User Profile', access_route: 'UserProfile88516'},
 { name: 'Tutorial88515', human_name: 'Tutorial', access_route: 'Tutorial88515', icon: 'question-circle'},
 { name: 'NotificationList88487', human_name: 'Notification List', access_route: 'NotificationList88487'},
-{ name: 'Settings88486', human_name: 'Settings', access_route: 'Settings88486'},
 { name: 'Settings88478', human_name: 'Settings', access_route: 'Settings88478'},
 { name: 'UserProfile88476', human_name: 'User Profile', access_route: 'UserProfile88476'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -17,7 +17,6 @@ import ArticleList91983Navigator from '../features/ArticleList91983/navigator';
 import UserProfile88516Navigator from '../features/UserProfile88516/navigator';
 import Tutorial88515Navigator from '../features/Tutorial88515/navigator';
 import NotificationList88487Navigator from '../features/NotificationList88487/navigator';
-import Settings88486Navigator from '../features/Settings88486/navigator';
 import Settings88478Navigator from '../features/Settings88478/navigator';
 import UserProfile88476Navigator from '../features/UserProfile88476/navigator';
 
@@ -40,7 +39,6 @@ ArticleList91983: { screen: ArticleList91983Navigator },
 UserProfile88516: { screen: UserProfile88516Navigator },
 Tutorial88515: { screen: Tutorial88515Navigator },
 NotificationList88487: { screen: NotificationList88487Navigator },
-Settings88486: { screen: Settings88486Navigator },
 Settings88478: { screen: Settings88478Navigator },
 UserProfile88476: { screen: UserProfile88476Navigator },
 
